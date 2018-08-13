628739754f9fd1e93ac9651ac83e3fe3d94c30ee
Kamyar Mohajerani <kammoh@gmail.com>
2018-08-13 16:00:30 -0400

    fix script

--- load.sh
+++ load.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-sudo insmod ./msr-pulsar.ko
+#!/bin/bash
+P=$(dirname $0)
+echo 1 | sudo tee /sys/bus/event_source/devices/cpu/rdpmc
+echo 2 | sudo tee /sys/bus/event_source/devices/cpu/rdpmc
+sudo insmod ${P}/msr-pulsar.ko
 sudo chmod a+rw /dev/cpu/*/pulsar_msr
 sudo chmod a+rw /dev/cpu/msr_batch
